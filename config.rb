316d795f34492f0937bc6f85cd37ed4cd4ece363
Eckhard Rotte <erotte@gmail.com>
2013-02-06 11:28:46 +0100

    layout, ruby-version, content

--- config.rb
+++ config.rb
@@ -55,6 +55,8 @@ set :js_dir, 'js'
 
 set :images_dir, 'images'
 
+set :slim, pretty: true
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
